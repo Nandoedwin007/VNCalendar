9172db17fb123ddf779fde5a6686a9f328a48f76
Edwin Coronado <cor14148@uvg.edu.gt>
2019-03-05 18:50:28 -0600

    Versión 1 con componentes visuales

---
+++ app/src/main/java/com/example/vncalendar/AbrirCalendario.kt
@@ -0,0 +1,12 @@
+package com.example.vncalendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AbrirCalendario : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_abrir_calendario)
+    }
+}
---
+++ app/src/main/java/com/example/vncalendar/AgregarActividad.kt
@@ -0,0 +1,12 @@
+package com.example.vncalendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgregarActividad : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_actividad)
+    }
+}
