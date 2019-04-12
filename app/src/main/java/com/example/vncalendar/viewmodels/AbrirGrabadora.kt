2ac325fde844f1e2cc7e7bd3f5697fea7f11cadc
JoeBlock99 <40869458+JoeBlock99@users.noreply.github.com>
2019-04-12 09:59:25 -0600

    blalala

---
+++ app/src/main/java/com/example/vncalendar/viewmodels/AbrirGrabadora.kt
@@ -0,0 +1,13 @@
+package com.example.vncalendar.viewmodels
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vncalendar.R
+
+class AbrirGrabadora : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_abrir_grabadora)
+    }
+}
---
+++ app/src/main/java/com/example/vncalendar/viewmodels/Recording.kt
@@ -0,0 +1,5 @@
+package com.example.vncalendar.viewmodels
+
+import java.io.Serializable
+
+class Recording(var uri: String, var fileName: String, var isPlaying: Boolean) :Serializable {}
